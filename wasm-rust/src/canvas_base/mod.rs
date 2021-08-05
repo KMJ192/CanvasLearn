b28a0994194afae4d0946addd2003b5d3ac60193
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2021-08-05 21:46:38 +0900

    canvas

--- wasm-rust/src/canvas_base/mod.rs
+++ wasm-rust/src/canvas_base/mod.rs
@@ -2,8 +2,6 @@ use std::f64;
 use wasm_bindgen::JsCast;
 
 pub fn canvas_base(canvas: web_sys::HtmlCanvasElement) {
-  //let context: CanvasRenderingContext2d = canvas.get_context("2d").unwrap();
-
   let context = canvas
     .get_context("2d")
     .unwrap()
@@ -11,7 +9,20 @@ pub fn canvas_base(canvas: web_sys::HtmlCanvasElement) {
     .dyn_into::<web_sys::CanvasRenderingContext2d>()
     .unwrap();
 
+  // let canvas_width: u32 = canvas.width();
+  // let canvas_height: u32 = canvas.height();
+  // let font_height: u32 = 15;
+  // let margin: u32 = 105;
+  // let hand_truncation: u32 = canvas_width;
+  // let hour_hand_truncation: u32 = canvas_width / 5;
+  // let numeral_spacing: u32 = 20;
+  // let radius = (canvas_width / 2) - margin as u32;
+  // let hand_radius: u32 = radius + numeral_spacing;
+
   context.begin_path();
+  // fn draw_cicle(context: &web_sys::CanvasRenderingContext2d){
+  //   context.arc((canvas_width / 2) as f64, canvas_height / 2, f64::from(*radius), 0.0, f64::consts::PI);
+  // }  
 
   fn calculation(context: &web_sys::CanvasRenderingContext2d){
     // Draw the outer circle.
@@ -42,5 +53,4 @@ pub fn canvas_base(canvas: web_sys::HtmlCanvasElement) {
 
   calculation(&context);
   draw(&context);
-        
 }
