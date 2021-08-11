d0f4611d2c2a9e4a8153727ab7355b940de95e3a
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2021-08-12 00:18:14 +0900

    마우스 drawing 이벤트

--- src/components/EventProcess/EventProcess.tsx
+++ src/components/EventProcess/EventProcess.tsx
@@ -1,13 +1,90 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, useCallback } from 'react';
 import { Redirect } from 'react-router';
 import styles from './EventProcess.module.scss';
 
+interface Coordinate { 
+  x: number;
+  y: number;
+}
+
 function EventProcess() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
   const [page, setPage] = useState({
     home: false,
     next: false
-  })
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+  });
+  const [mousePosition, setMousePosition] = useState<Coordinate | undefined>(undefined);
+  const [isPainting, setIsPainting] = useState(false);
+
+  const getCoordinates = (e: MouseEvent): Coordinate | undefined => {
+    return {
+      x: e.offsetX - 95,
+      y: e.offsetY - 90
+    }
+  }
+
+  const drawLine = (originMousePos: Coordinate, newMousePos: Coordinate) => {
+    if (!canvasRef.current) {
+      return;
+    }
+    const canvas: HTMLCanvasElement = canvasRef.current;
+    const context = canvas.getContext('2d');
+    if (context) {
+      context.strokeStyle = 'red';
+      context.lineJoin = 'round';
+      context.lineWidth = 1;
+
+      context.beginPath();
+      context.moveTo(originMousePos.x,  originMousePos.y);
+      context.lineTo(newMousePos.x, newMousePos.y);
+      context.closePath();
+
+      context.stroke();
+    }  
+  };
+
+  const startPaint = useCallback((e: MouseEvent) => { 
+    const coordinates = getCoordinates(e);
+    if (coordinates) {
+      setIsPainting(true);
+      setMousePosition(coordinates);
+    }
+  }, [isPainting, mousePosition]);
+
+  const painting = useCallback((e: MouseEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (isPainting) {
+      const newMousePosition: Coordinate | undefined = getCoordinates(e);
+      if(mousePosition && newMousePosition) {
+        drawLine(mousePosition, newMousePosition);
+        setMousePosition(newMousePosition);
+      }
+    }
+  }, [isPainting, mousePosition]);
+
+  const exitPaint = useCallback(() => {
+    setIsPainting(false);
+  }, [isPainting]);
+  
+  useEffect(() => {
+    if (!canvasRef.current) {
+      return;
+    }
+    const canvas: HTMLCanvasElement | null = canvasRef.current;
+    canvas.addEventListener('mousedown', startPaint);
+    canvas.addEventListener('mousemove', painting);
+    canvas.addEventListener('mouseup', exitPaint);
+    canvas.addEventListener('mouseleave', exitPaint);
+    
+    return () => {
+      canvas.removeEventListener('mousedown', startPaint);
+      canvas.removeEventListener('mousemove', painting);
+      canvas.removeEventListener('mouseup', exitPaint);
+      canvas.removeEventListener('mouseleave', exitPaint);
+    }
+  }, [startPaint, painting, canvasRef]);
 
   const nextScreen = () => {
     setPage({
